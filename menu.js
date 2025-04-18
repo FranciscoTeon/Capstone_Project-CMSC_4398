1beaf7d0502c910ba92d16e0af184e7a895ffa41
FranciscoTeon <90804433+FranciscoTeon@users.noreply.github.com>
2025-04-18 13:04:37 -0700

    Third Commit

---
+++ menu.js
@@ -0,0 +1,9 @@
+function showBurger(){
+    const hamburger = document.querySelector('.hamburger')
+        hamburger.style.display = 'flex'
+}
+
+function hideBurger(){
+    const hamburger = document.querySelector('.hamburger')
+        hamburger.style.display = 'none'
+}
