9395e72d3888b0aeb768c557f682ebd65aa9e3bb
FranciscoTeon <90804433+FranciscoTeon@users.noreply.github.com>
2025-04-18 21:49:06 -0700

    fourth commit

--- app.js
+++ app.js
@@ -6,3 +6,21 @@ faqs.forEach(faq => {
 
     });
 });
+
+// const questions = document.querySelectorAll('.faq .question');
+
+//   questions.forEach(question => {
+//     question.addEventListener('click', () => {
+//       const currentFaq = question.parentElement;
+
+//       // Close all FAQs first
+//       document.querySelectorAll('.faq').forEach(faq => {
+//         if (faq !== currentFaq) {
+//           faq.classList.remove('active');
+//         }
+//       });
+
+//       // Toggle current FAQ
+//       currentFaq.classList.toggle('active');
+//     });
+//   });
